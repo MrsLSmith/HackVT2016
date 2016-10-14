ec788d1ae133c45a839f0a9bb5c17016da4ee6cd
John Need <johnneed@johnneed.com>
2016-10-14 19:17:28 -0400

    demo app

---
+++ the-hive/imports/ui/body.js
@@ -0,0 +1,10 @@
+import { Template } from 'meteor/templating';
+
+import './body.html';
+import { Tasks } from '../api/tasks.js';
+
+Template.body.helpers({
+    tasks() {
+        return Tasks.find({});
+    },
+});
