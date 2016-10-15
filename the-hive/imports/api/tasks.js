e311f3ffd2063309cb39f189efca560ac13ff05c
David-STEM <dstpierre@ccsuvt.org>
2016-10-15 04:41:57 -0400

    stuff

--- the-hive/imports/api/tasks.js
+++ the-hive/imports/api/tasks.js
@@ -4,4 +4,4 @@ export const Tasks = new Mongo.Collection('tasks');
 export const Locations = new Mongo.Collection('locations');
 export const Users = new Mongo.Collection('users');
 export const Groups = new Mongo.Collection('groups');
-export const Topics = new Mongo.Collection('topics');
+export const Schools = new Mongo.Collection('schools');
--- the-hive/imports/ui/body.js
+++ the-hive/imports/ui/body.js
@@ -1,11 +1,20 @@
 import { Template } from 'meteor/templating';
 
 import './body.html';
-import { Users } from '../api/tasks.js';
-import { Location } from '../api/tasks.js';
+import { Users, Locations, Groups, Schools } from '../api/tasks.js';
 
 Template.body.helpers({
     users() {
         return Users.find({});
     },
+    locations() {
+        return Locations.find({});
+    },
+    groups() {
+        return Groups.find({});
+    },
+    schools() {
+        return Schools.find({});
+    },
+
 });
